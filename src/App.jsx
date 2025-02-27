96ea6aab1ceef02fd15ba2ae7dabe644a57d53b5
heuristic_machina <machine_learner@yahoo.com>
2025-02-27 03:17:02 -0700

    video background added to home component

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Card from './components/card'
 
 
 const App = () => {
-  return (
-    <div className='container p-12 md:p-14 lg:p-16 xl:p-20 m-7 flex flex-col'>
-      {/* <Home /> */}
-      <Card />
-      <Header />
-      <h1 className='mt-8 text-2xl text-center mb-8'>Digitize Your Business</h1>
-      <img className='rounded-md' src="https://www.shutterstock.com/shutterstock/videos/3705393675/thumb/10.jpg?ip=x480" alt='' />
-      <Section1 />
-      <Section2 />
- </div>
+  return ( 
+  <>
+      <div className='container p-8 md:p-14 lg:p-16 xl:p-20 m-7 flex flex-col'>
+        <Header />
+        <Home />
+        <Card />
+        <h1 className='mt-8 text-2xl text-center mb-8'>Digitize Your Business</h1>
+        <img className='rounded-md' src="https://www.shutterstock.com/shutterstock/videos/3705393675/thumb/10.jpg?ip=x480" alt='' />
+        <Section1 />
+        <Section2 />
+      </div>
+  </>
   )
 };
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,20 @@
-// import styles from '../App.css';
-
 const Home = () => {
     return (
-        <div className={styles.main}>
-            <div className={styles.contents}>
-                <h1>JOIN THE FIRST</h1>
-                <h1>INDUSTRIAL MACHINERY</h1>
-                <h1>ECOSYSTEM!</h1>
+        <div className='main'>
+            <div className='contents'>
+                <h1>JOIN THE FIRST GLOBAL
+                INDUSTRIAL MACHINERY
+                ECOSYSTEM!</h1>
             </div>
             <div>
-                <p>Become a part of the first global marketplace that covers your special needs within the machine and plant engineering industry and digitize your business.</p>
+                <p className="text-xs px-36 mb-20">Become a part of the first global marketplace that covers your special needs within the machine and plant engineering industry and digitize your business.</p>
             </div>
             <video 
-            src='./assets/video.mp4'
+            src='/video.mp4'
             autoPlay 
             muted 
             loop
-            className={styles.video}
+            className='video'
             />
         </div>
     )
